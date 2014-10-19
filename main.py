752d3b5c1301d4acd1539831fd03abd63ac19cf5
Philipp Schrader <philipp.schrader@gmail.com>
2014-10-19 15:33:27 -0700

    Switch between two different lists.

--- main.py
+++ main.py
@@ -18,22 +18,32 @@ class VMWidget (urwid.WidgetWrap):
     def keypress(self, size, key):
         return key
 
+def switch_list(listbox):
+    global current_listbox
+    current_listbox = listbox
+    main.contents.update(body=(listbox, None))
 
 def handle_input(key):
     if key in ('q', 'Q'):
         raise urwid.ExitMainLoop()
 
-    elif key in ('j'):
+    elif key in ('j',):
         try:
-            listbox.set_focus(listbox.focus_position + 1)
+            current_listbox.set_focus(current_listbox.focus_position + 1)
         except IndexError:
             pass
 
-    elif key in ('k'):
+    elif key in ('k',):
         try:
-            listbox.set_focus(listbox.focus_position - 1)
+            current_listbox.set_focus(current_listbox.focus_position - 1)
         except IndexError:
             pass
+
+    elif key in ('e',):
+        if current_listbox is listbox_props:
+            switch_list(listbox_vms)
+        else:
+            switch_list(listbox_props)
 
 palette = [
         ('highlight', 'black', 'brown'),
@@ -43,12 +53,19 @@ palette = [
 
 vms = 'foo lkj kj lskjdf jlklekj f'.split()
 
+listbox_vms = urwid.ListBox(urwid.SimpleListWalker( \
+        [VMWidget('foo', v) for v in vms]))
+listbox_props = urwid.ListBox(urwid.SimpleListWalker( \
+        [VMWidget('barr', v + ' me') for v in vms]))
+
+current_listbox = listbox_vms
+
 shortcuts = urwid.AttrMap(urwid.Text(' q: Quit'), 'highlight')
-listbox = urwid.ListBox(urwid.SimpleListWalker([VMWidget('foo', v) for v in vms]))
+listbox_vms_map = urwid.AttrMap(listbox_vms, 'body')
+listbox_props_map = urwid.AttrMap(listbox_props, 'body')
 label = urwid.AttrMap(urwid.Text(' VM Selection'), 'highlight')
 
-main = urwid.Frame(urwid.AttrMap(listbox, 'body'), header=shortcuts, \
-        footer=label)
+main = urwid.Frame(listbox_vms_map, header=shortcuts, footer=label)
 
 loop = urwid.MainLoop(main, palette=palette, unhandled_input=handle_input)
 loop.screen.set_terminal_properties(colors=16)
